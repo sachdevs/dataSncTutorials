a8ca5dce6d35136ad85f2688a2abb1c6a13955bd
sachdevs <sachdev.96@gmail.com>
2015-05-15 13:30:43 -0400

    further changes

--- sql/ex6.sql
+++ sql/ex6.sql
@@ -3,3 +3,4 @@ SELECT pet.id, pet.name, pet.age, pet.dead FROM pet, person_pet, person
 		pet.id = person_pet.pet_id AND
 		person_pet.person_id = person.id AND
 		person.first_name = "Zed";
+SELECT * FROM pet;
