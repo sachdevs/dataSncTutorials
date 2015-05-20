1a4d06cc66622e6c474a6a5c4f77eb0fa323170d
sachdevs <sachdev.96@gmail.com>
2015-05-20 15:23:14 -0400

    twitter continues

--- twitSent/main.py
+++ twitSent/main.py
@@ -1,5 +1,22 @@
 import tweepy
 from loginDetails import consumer_key, consumer_secret, access_token, access_token_secret
+import warnings
+warnings.filterwarnings("ignore")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text.encode('utf-8')
+
+print '\n\n\n'
+
+user = api.get_user('twitter')
+
+print user.screen_name
+print user.followers_count
+for friend in user.friends():
+   print friend.screen_name
