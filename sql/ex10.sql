6ff62861db3b63d46bba7d5f5ed5db3b4ffed41a
sachdevs <sachdev.96@gmail.com>
2015-05-20 13:41:01 -0400

    ex10

---
+++ sql/ex10.sql
@@ -0,0 +1,11 @@
+SELECT * FROM pet;
+
+UPDATE pet SET name = "Zed's Pet" WHERE id in(
+	SELECT pet.id FROM pet, person_pet, person
+	WHERE 
+	person_pet.person_id = person.id AND
+	pet.id = person_pet.pet_id AND
+	person.first_name = "Zed"
+);
+
+SELECT * FROM pet;
